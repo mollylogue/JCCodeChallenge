8ca460605fe56696416c0b25563359621c616777
mollylogue <molly@deepfield.net>
2017-06-25 19:39:01 -0400

    factor encoding of password into separate function

--- server/server.go
+++ server/server.go
@@ -7,19 +7,26 @@ import (
     "encoding/base64"
         )
 
+func encode_password(password string) (string){
+    hasher := sha512.New()
+    bv := []byte(password) 
+    hasher.Write(bv)
+    sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+    return sha
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+        
         r.ParseForm()
         password := r.Form.Get("password")
-        hasher := sha512.New()
-        bv := []byte(password) 
-        hasher.Write(bv)
-        sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
-        fmt.Fprintf(w, "Password normal %s", password)
-        fmt.Fprintf(w, "Password encoded %s", sha)
+        password = encode_password(password)
+
+        fmt.Fprintf(w, "Password encoded %s", password)
         
 }
 
 func main() {
+
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
     
