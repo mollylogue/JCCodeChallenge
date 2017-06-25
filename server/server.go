253d42c3db9ff7fb09b58726806c4491cb09de16
mollylogue <molly@deepfield.net>
2017-06-25 19:05:11 -0400

    use sha512 hashing algorithm to hash password, encode base 64

--- server/server.go
+++ server/server.go
@@ -3,11 +3,20 @@ package main
 import (
     "fmt"
     "net/http"
+    "crypto/sha512"
+    "encoding/base64"
         )
 
 func handler(w http.ResponseWriter, r *http.Request) {
         r.ParseForm()
-        fmt.Fprintf(w, "Password %s", r.Form.Get("password"))
+        password := r.Form.Get("password")
+        hasher := sha512.New()
+        bv := []byte(password) 
+        hasher.Write(bv)
+        sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+        fmt.Fprintf(w, "Password normal %s", password)
+        fmt.Fprintf(w, "Password encoded %s", sha)
+        
 }
 
 func main() {
