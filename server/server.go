b0e8d8f80b7905e2f2d05c118962d6033e836484
mollylogue <molly@deepfield.net>
2017-06-27 21:59:47 -0400

    add explanatory comments

--- server/server.go
+++ server/server.go
@@ -12,6 +12,7 @@ import (
         )
 
 func encode_password(password string) (string){
+    // Hash password & encode
     hasher := sha512.New()
     bv := []byte(password) 
     hasher.Write(bv)
@@ -20,6 +21,7 @@ func encode_password(password string) (string){
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    // Wait 5 seconds before responding
     time.Sleep(5 * time.Second)
  
     logger := log.New(os.Stdout, "", 0)
@@ -27,6 +29,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
     password := r.Form.Get("password")
     password = encode_password(password)
 
+    // Would want to store this in a database, but printing for the
+    // purposes of this challenge. 
     logger.Println("Password encoded ", password)
         
 }
@@ -34,15 +38,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 
     logger := log.New(os.Stdout, "", 0)
+ 
     stop := make(chan os.Signal, 1)
     signal.Notify(stop, os.Interrupt)
     port := ":8080"
 
+    // Create a new server
     s := &http.Server{
                         Addr:           port,
                         Handler:        http.HandlerFunc(handler),
                         }
 
+    // Start a goroutine
     go func(){
         logger.Printf("Listening on port %s...", port)
         if err := s.ListenAndServe() ; err != nil {
@@ -54,6 +61,7 @@ func main() {
 
     ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 
+    // Use Shutdown module for a graceful shutdown
     s.Shutdown(ctx)
 
     }
